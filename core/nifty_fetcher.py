113a78ca102bde089fcd547e65d9b0a6f1e539f9
Thrinath Reddy <thrinathreddyg@gmail.com>
2025-06-30 21:04:32 +0530

    sc

--- core/nifty_fetcher.py
+++ core/nifty_fetcher.py
@@ -7,25 +7,28 @@ from datetime import datetime, timedelta
 logging.basicConfig(level=logging.INFO)
 
 def fetch_data(symbol, days=180):
-    end = datetime.today()
-    start = end - timedelta(days=days)
-    """
-    Download data for a symbol with retries and error handling.
-    """
+    try:
+        end = datetime.today()
+        start = end - timedelta(days=days)
+        """
+        Download data for a symbol with retries and error handling.
+        """
 
-    for attempt in range(1, retries + 1):
-        try:
-            data = get_history(symbol=symbol, start=start, end=end)
-            logging.warning(data)
-            if data is not None and not data.empty:
-                logging.warning(f"[{symbol}] not empty data")
-                return data
-            else:
-                logging.warning(f"[{symbol}] Empty data on attempt {attempt}. Retrying...")
-        except Exception as e:
-            logging.error(f"[{symbol}] Exception on attempt {attempt}: {e}")
+        for attempt in range(1, retries + 1):
+            try:
+                data = get_history(symbol=symbol, start=start, end=end)
+                logging.warning(data)
+                if data is not None and not data.empty:
+                    logging.warning(f"[{symbol}] not empty data")
+                    return data
+                else:
+                    logging.warning(f"[{symbol}] Empty data on attempt {attempt}. Retrying...")
+            except Exception as e:
+                logging.error(f"[{symbol}] Exception on attempt {attempt}: {e}")
 
-        sleep(delay)
+            sleep(delay)
 
-    logging.error(f"[{symbol}] Failed to fetch data after {retries} attempts.")
-    return None
+        logging.error(f"[{symbol}] Failed to fetch data after {retries} attempts.")
+        return None
+    except Exception as e:
+    logging.error(f"Exception: {e}")
