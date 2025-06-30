cbb74bc5be678cfb00db1183149a07de175456a0
Thrinath Reddy <thrinathreddyg@gmail.com>
2025-06-30 20:59:48 +0530

    sc

--- core/nifty_fetcher.py
+++ core/nifty_fetcher.py
@@ -2,20 +2,19 @@ import yfinance as yf
 from nsepy import get_history
 import logging
 from time import sleep
-import datetime
+from datetime import datetime, timedelta
 
 logging.basicConfig(level=logging.INFO)
 
-def fetch_data(symbol, period="6mo", interval="1d", retries=3, delay=2):
+def fetch_data(symbol, days=180):
+    end = datetime.today()
+    start = end - timedelta(days=days)
     """
     Download data for a symbol with retries and error handling.
     """
+
     for attempt in range(1, retries + 1):
         try:
-            if start is None:
-                start = datetime.date.today() - datetime.timedelta(days=180)
-            if end is None:
-                end = datetime.date.today()
             data = get_history(symbol=symbol, start=start, end=end)
             logging.warning(data)
             if data is not None and not data.empty:
