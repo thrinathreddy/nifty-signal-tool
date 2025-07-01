77eb60579eb25aaba35192225e5aed89f7f9f3a3
Thrinath Reddy <thrinathreddyg@gmail.com>
2025-07-01 12:54:11 +0530

    sc

--- core/strategy.py
+++ core/strategy.py
@@ -1,7 +1,8 @@
 def generate_signal(df):
-    latest = df.iloc[-1]
-    if latest["rsi"] < 30 and latest["macd"] > latest["macd_signal"]:
-        return "BUY"
-    elif latest["rsi"] > 70 and latest["macd"] < latest["macd_signal"]:
-        return "SELL"
-    return "HOLD"
+    #latest = df.iloc[-1]
+    #if latest["rsi"] < 30 and latest["macd"] > latest["macd_signal"]:
+    #    return "BUY"
+    #elif latest["rsi"] > 70 and latest["macd"] < latest["macd_signal"]:
+    #    return "SELL"
+    #return "HOLD"
+    return "BUY"
