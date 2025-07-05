eabf839c20b88465a0721d23a67171f499510ef6
Thrinath Reddy <thrinathreddyg@gmail.com>
2025-07-05 14:52:42 +0530

    sc

--- strategies/macd_histogram.py
+++ strategies/macd_histogram.py
@@ -1,10 +1,13 @@
 def generate_signal(df):
-    if "macd_hist" not in df.columns:
+    if "macd_hist" not in df.columns or len(df) < 2:
         return "HOLD"
+
     prev = df.iloc[-2]
     curr = df.iloc[-1]
+
     if prev["macd_hist"] < 0 and curr["macd_hist"] > 0:
         return "BUY"
     elif prev["macd_hist"] > 0 and curr["macd_hist"] < 0:
         return "SELL"
+
     return "HOLD"
