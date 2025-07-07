536d7db5929be9ac71a937ca4281b8c715c49bc2
thrinathreddy <thrinathreddyg@gmail.com>
2025-07-07 21:57:49 +0530

    sc

--- strategies/momentum_trend_volume.py
+++ strategies/momentum_trend_volume.py
@@ -8,11 +8,11 @@ def generate_signal(df):
     latest = df.iloc[-1]
     avg_volume = df["volume"].rolling(20).mean().iloc[-1]
 
-    if pd.isna(latest["rsi"]) or pd.isna(latest["ema50"]) or pd.isna(avg_volume):
+    if pd.isna(latest["rsi"]) or pd.isna(latest["ema_50"]) or pd.isna(avg_volume):
         return "HOLD"
 
-    if latest["close"] > latest["ema50"] and latest["rsi"] > 60 and latest["volume"] > avg_volume:
+    if latest["close"] > latest["ema_50"] and latest["rsi"] > 60 and latest["volume"] > avg_volume:
         return "BUY"
-    elif latest["close"] < latest["ema50"] and latest["rsi"] < 40 and latest["volume"] > avg_volume:
+    elif latest["close"] < latest["ema_50"] and latest["rsi"] < 40 and latest["volume"] > avg_volume:
         return "SELL"
     return "HOLD"
