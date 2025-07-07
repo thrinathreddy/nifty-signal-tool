bea1baf49a70f72b314c75bbcf7b0ebd4f5ad57e
thrinathreddy <thrinathreddyg@gmail.com>
2025-07-07 19:57:51 +0530

    sc

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -199,7 +199,7 @@ with tab4:
         if trades:
             df_bt = pd.DataFrame(trades, columns=[
                 "Date", "Signal", "Buy", "Sell",
-                "Gross PnL", "Brokerage", "GST", "Net PnL"
+                "Gross PnL", "Brokerage", "GST", "STT", "Other chrgs", "Net PnL"
             ])
             df_bt["Date"] = pd.to_datetime(df_bt["Date"])
             df_bt["Cumulative Net PnL"] = df_bt["Net PnL"].cumsum()
